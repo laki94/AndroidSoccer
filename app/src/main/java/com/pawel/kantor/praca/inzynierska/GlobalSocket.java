a45909f829ab8289c5095d029f74585b144ad522
Pawel <pawel190x@gmail.com>
2017-12-18 15:43:06 +0100

    Naprawa

---
+++ app/src/main/java/com/pawel/kantor/praca/inzynierska/GlobalSocket.java
@@ -0,0 +1,57 @@
+package com.pawel.kantor.praca.inzynierska;
+
+import android.app.Application;
+import android.os.Handler;
+
+import com.pawel.kantor.praca.inzynierska.Bluetooth.BluetoothConnectionService;
+
+/**
+ * Created by Pawel on 29.11.2017.
+ */
+
+public class GlobalSocket extends Application {
+
+    private BluetoothConnectionService mBluetoothConnectionService;
+    private Handler mBluetoothHandler;
+    private Handler mBluetoothGameHandler;
+    private boolean amIConnected = false;
+
+    public BluetoothConnectionService getBluetoothConnectionService() {
+        return mBluetoothConnectionService;
+    }
+
+    public void setBluetoothConnectionService(BluetoothConnectionService mBluetoothConnectionService) {
+        this.mBluetoothConnectionService = mBluetoothConnectionService;
+    }
+
+    public void setBluetoothHandler(Handler handler)
+    {
+        mBluetoothHandler = handler;
+    }
+
+    public Handler getBluetoothHandler()
+    {
+        return mBluetoothHandler;
+    }
+
+    public void setAmIConnected(boolean state)
+    {
+        amIConnected = state;
+    }
+
+    public boolean getAmIConnected()
+    {
+        return amIConnected;
+    }
+
+    public void setBluetoothGameHandler(Handler handler)
+    {
+        mBluetoothGameHandler = handler;
+    }
+
+    public Handler getBluetoothGameHandler()
+    {
+        return mBluetoothGameHandler;
+    }
+
+}
